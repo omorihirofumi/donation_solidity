bf60f229c0f0e890214aa7597766073098a5c9a3
omorihirofumi <kanazawabunko827@icloud.com>
2020-03-25 15:33:52 +0900

    サイドバー部分追加

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// react-bootstarpのライブラリを使用する
+import { Container, Navbar } from 'react-bootstrap';
+
+// Appコンポーネントを定義する
+class App extends Component {
+
+  // renderの中身がWebページに描画（レンダリング）されます。
+  render() {
+    return (
+      <div>
+        <Container>
+          <Navbar bg="primary" variant="dark">
+            <Navbar.Brand href="#home">寄付アプリ</Navbar.Brand>
+          </Navbar>
+        </Container>
+        <ul class="navbar-nav mr-auto">
+         <li class="nav-item">
+          <a class="nav-link" href="main.html">メイン画面<span class="sr-only">(current)</span></a>
+         </li>
+         <li class="nav-item">
+          <a class="nav-link" href="send.html">送金画面</a>
+         </li>
+         <li class="nav-item active">
+          <a class="nav-link" href="register.html">会員登録</a>
+         </li>
+        </ul>
+      </div>
+    );
+
+    return(
+      <div class="col-md-6">
+        <div class="form-group">
+         <label for="userName">ユーザー名を入力して下さい。</label>
+         <input type="text" class="form-control" id="userName" placeholder="">
+        </div>
+        <div class="form-group">
+         <label for="userEmail">Eメールアドレスを入力して下さい。</label>
+         <input type="email" class="form-control" id="userEmail" placeholder="">
+        </div>
+       <p><button onclick="registerAccount()" class="btn btn-default">登録</button></p><br>
+      </div>
+    )
+  }
+  
 }
 
+// 外部読み込みを可能にする
 export default App;
