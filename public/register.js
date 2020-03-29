06abc0f6cd2f67e4a1329bb3e09f85b11eb32b24
omorihirofumi <kanazawabunko827@icloud.com>
2020-03-30 08:47:16 +0900

    main.js一覧表示追加

--- public/register.js
+++ public/register.js
@@ -2,6 +2,7 @@
       // テキストボックスから入力内容を取得する
       var userName = document.getElementById("name").value;
       var userEmail = document.getElementById("email").value;
+      
   
       // コントラクトの呼び出し
       return contract.methods.registerAccount(userName, userEmail).send({ from: coinbase });
