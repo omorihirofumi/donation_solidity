3cd3d53c8db1775959b50b80c4ca2b8416d9de98
omorihirofumi <kanazawabunko827@icloud.com>
2020-03-30 12:33:45 +0900

    firebase追加

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC4nQRoLGU3AYyNzd_d5L0NvcPbBCFOXbY",
+  authDomain: "blockchain-donation.firebaseapp.com",
+  databaseURL: "https://blockchain-donation.firebaseio.com",
+  projectId: "blockchain-donation",
+  storageBucket: "blockchain-donation.appspot.com",
+  messagingSenderId: "849741540435",
+  appId: "1:849741540435:web:019edc09ce7fdf26c86aa6",
+  measurementId: "G-Q8TTZ476YE"
+ };
